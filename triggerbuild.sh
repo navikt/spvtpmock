ce5a150fe952c45df308b373932e04b3eeb19c7e
tuantrannav <dinh.anh.tuan.tran@nav.no>
2019-02-26 10:26:54 +0100

    added a script to trigger other builds

--- triggerbuild.sh
+++ triggerbuild.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh
-echo "Trigger build....."
-output=$(curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Travis-API-Version: 3' -H 'Authorization: token ${TRAVIS_API_TOKEN}' -d '{"request": {"branch":"master"}}' 'https://api.travis-ci.com/repo/navikt%2Fhelse-e2e/requests')
+#!/bin/sh
+PROJECT=$@
+echo "Trigger build $PROJECT"
+output=$(curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_API_TOKEN" -d "{\"request\": {\"branch\":\"master\"}}" "https://api.travis-ci.com/repo/navikt%2F$PROJECT/requests")
 echo $output
